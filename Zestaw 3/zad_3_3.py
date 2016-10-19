be965792e7dd7edf2389c1d0f26df226446f8616
manoftheoak <jakub.jas@uj.edu.pl>
2016-10-20 01:22:00 +0200

    Added: 3.3, 3.4

---
+++ Zestaw 3/zad_3_3.py
@@ -0,0 +1,3 @@
+for num in range(30):
+    if num % 3 != 0:
+        print num
---
+++ Zestaw 3/zad_3_4.py
@@ -0,0 +1,12 @@
+while True:
+
+    reply = raw_input("Podaj liczbe rzeczywista: ")
+
+    if reply == "stop":
+        break
+    try:
+        number = float(reply)
+    except ValueError:
+        print "To nie jest liczba!"
+    else:
+        print pow(number, 3)
